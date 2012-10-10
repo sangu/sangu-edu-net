0d92e172f9614a8c93f972e541d9707e79dad299
nikoloz-pachuashvili <nikoloz@hotmail.com>
2012-10-10 13:20:32 +0400

    Char extension (IsGeorgianLetter)

---
+++ ProjectSangu/ProjectSangu/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectSangu
+{
+    public static class Extensions
+    {
+        public static bool IsGeorgianLetter(this char value)
+        {
+            return value >= 'ა' && value <= 'ჵ';
+        }
+    }
+}
